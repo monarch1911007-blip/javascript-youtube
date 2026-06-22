a035375bc7e6d9c31240468b451b63eb51808570
monarch1911007-blip <monarch1911007@gmail.com>
2026-06-22 11:03:42 +0000

    datatypes and variables

---
+++ variable.js
@@ -0,0 +1,16 @@
+const accountId =144567
+let accountEmail="ateeb1234@gmail.com"
+var accountpassword="razer@123456"
+accountCity="Lahore"
+
+accountEmail="email@1234gmail.com"
+accountpassword="12345678"
+accountCity="islamabad"
+
+
+console.log(accountCity);
+
+
+console. table([ accountEmail, accountpassword, accountCity])
+
+
