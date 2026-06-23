269e4184caebf225ca6cd5e09dc5e6578c5ab030
monarch1911007-blip <monarch1911007@gmail.com>
2026-06-23 07:10:33 +0000

    conversion of datatpes

---
+++ datatype-conversion.js
@@ -0,0 +1,33 @@
+// let score = "monarch"
+
+//  console.log( typeof score);
+//  console.log( typeof (score));
+
+//  let valueInNumber = Number(score)
+//   console.log( typeof valueInNumber);
+
+//    console.log( valueInNumber);
+
+
+   /*
+   jab hum kisi number ki datatype ko convert 
+   krty hai to wo easily convert ho jati hai 
+   jaisa "45" to ya easily convert ho jay ga
+   */
+
+   /* jab hum nubmers k sath koi
+    alphabet bhi laga dety hai
+     jaisa "45abc" or jab hum 
+     isy print krway gy to 
+     ouput ay ga NaN means not a number
+      yani ya koi number nahi hai. */
+
+      /* boolean ma jab value true ho ge
+       to 1output ay ga or agr false ho ga
+       to 0 output ay ga */
+
+       let someNumber = "40"
+
+       let stringNumber = String(someNumber)
+       console.log (stringNumber);
+       console.log ( typeof stringNumber)
