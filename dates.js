b09568b6eb2daa0fdf8c24bf13732947519dc677
monarch1911007-blip <monarch1911007@gmail.com>
2026-07-01 07:25:02 +0000

    dates-in =-js.js

---
+++ dates.js
@@ -0,0 +1,32 @@
+// Dates
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+// let myCreatedDate = new Date(2026, 8, 1, 11, 19)
+// let myCreatedDate = new Date("2026-01-14");
+let myCreatedDate = new Date("01-14-2026");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+})
